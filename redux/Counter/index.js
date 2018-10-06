a751f6dd0b1ab598775fdc047ec5e59b128fd7b3
Thalyson <thalysonr.castro@hotmail.com>
2018-10-06 10:19:30 -0400

    Organizando

---
+++ redux/Counter/index.js
@@ -0,0 +1,5 @@
+import Counter from './counter';
+import CounterReducer from './counter.reducer';
+import CounterSagas from './counter.sagas';
+
+export { Counter, CounterReducer, CounterSagas};
--- redux/index.jsx
+++ redux/index.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import counterReducer from './Counter/counter.reducer';
-import Counter from './Counter/counter';
+import {applyMiddleware, compose, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import { Counter } from './Counter';
 import createSagaMiddleware from 'redux-saga';
-import rootSaga from './Counter/counter.sagas';
+import rootSaga from './sagas';
+import reducers from './reducer';
 
 const sagaMiddleware = createSagaMiddleware();
-
-const reducers = combineReducers({
-    counter: counterReducer
-});
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const store = createStore(
     reducers,
     composeEnhancers(
         applyMiddleware(sagaMiddleware)
     )
 );
-
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
