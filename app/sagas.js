8f0c88bc691269b4d4b7023ed3cd3f133d917dfe
Thalyson <ThalysonR.castro@hotmail.com>
2018-10-10 10:03:55 -0400

    Limpando

--- app/sagas.js
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects';
-import CounterSagas from './modules/Counter/counter.sagas';
-
-export default function* rootSaga() {
-    yield all([
-        CounterSagas()
-    ])
-}
